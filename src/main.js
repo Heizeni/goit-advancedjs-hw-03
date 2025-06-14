0851a0fad89d6a107b2b5367639927212a58bdef
Heizeni <valeriya.gonzha@gmail.com>
2025-06-14 16:52:35 +0300

    rev-3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { fetchImages } from './js/pixabay-api.js';
 import { clearGallery, renderImages } from './js/render-functions.js';
+import { showLoader, hideLoader } from './js/loader.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
@@ -15,7 +16,6 @@ const lightbox = new SimpleLightbox('.gallery a', {
 
 form.addEventListener('submit', async event => {
   event.preventDefault();
-
   const query = event.currentTarget.elements.searchQuery.value.trim();
 
   if (!query) {
@@ -27,23 +27,42 @@ form.addEventListener('submit', async event => {
   }
 
   clearGallery(gallery);
+  showLoader();
 
   try {
     const data = await fetchImages(query);
 
     if (data.hits.length === 0) {
       iziToast.info({
-        message: 'Sorry, there are no images matching your search query. Please try again!',
+        message: 'Sorry, there are no images matching your search request.',
         position: 'topRight',
       });
-    } else {
-      renderImages(data.hits, gallery);
-      lightbox.refresh();
+      hideLoader();
+      return;
     }
+
+    renderImages(data.hits, gallery);
+
+    const images = gallery.querySelectorAll('img');
+    const promises = Array.from(images).map(img => {
+      return new Promise(resolve => {
+        if (img.complete) {
+          resolve();
+        } else {
+          img.onload = () => resolve();
+          img.onerror = () => resolve();
+        }
+      });
+    });
+    await Promise.all(promises);
+
+    hideLoader();
+    lightbox.refresh();
   } catch (error) {
     iziToast.error({
       message: 'Something went wrong. Please try again later.',
       position: 'topRight',
     });
+    hideLoader();
   }
 });
